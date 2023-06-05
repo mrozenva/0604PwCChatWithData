490768ad1ab40fa442afba55a7211ba943426a90
mrozenva <mrozenva@gmail.com>
2023-06-05 12:14:40 -0400

    small change

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -4,6 +4,8 @@ import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
 const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
+Please provide all answers in bullet point format.
+
 Chat History:
 {chat_history}
 Follow Up Input: {question}
@@ -12,6 +14,7 @@ Standalone question:`;
 // change to your own 'system' prompt
 const QA_PROMPT = `You are an expert financial analyst. You are qualified and adept at seeing Form 10-K financial reports and extracting and interpreting key financial information out of them. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+Please provide all answers in bullet point format.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
@@ -32,7 +35,7 @@ export const makeChain = (vectorstore: PineconeStore, openaiApiKey: string) => {
     {
       qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+      returnSourceDocuments: true,
     },
   );
 
